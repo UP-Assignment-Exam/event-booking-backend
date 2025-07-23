2d5834a44d05f67c48f307a6977913e8cfad1692
DamonKert <125014031+DamonKert@users.noreply.github.com>
2025-07-23 14:31:59 -0400

    Fix right group

--- services/right-permission.service.js
+++ services/right-permission.service.js
@@ -2,65 +2,65 @@ const Right = require("../models/Rights.model");
 
 const rightsSeedData = [
     // Dashboard
-    { group: 'dashboard', group: 'dashboard', name: 'dashboard:view', description: 'View dashboard metrics and charts' },
+    { group: 'Dashboard', name: 'dashboard:view', description: 'View dashboard metrics and charts' },
 
-    // Event
-    { group: 'event', group: 'event', name: 'event:create', description: 'Create new events' },
-    { group: 'event', group: 'event', name: 'event:update', description: 'Update event details' },
-    { group: 'event', group: 'event', name: 'event:view', description: 'View event information' },
-    { group: 'event', group: 'event', name: 'event:delete', description: 'Delete events' },
+    // EvenE
+    { group: 'Event', name: 'event:create', description: 'Create new events' },
+    { group: 'Event', name: 'event:update', description: 'Update event details' },
+    { group: 'Event', name: 'event:view', description: 'View event information' },
+    { group: 'Event', name: 'event:delete', description: 'Delete events' },
 
     // Category
-    { group: 'category', group: 'category', name: 'category:create', description: 'Create new categories' },
-    { group: 'category', group: 'category', name: 'category:update', description: 'Update categories' },
-    { group: 'category', group: 'category', name: 'category:view', description: 'View categories' },
-    { group: 'category', group: 'category', name: 'category:delete', description: 'Delete categories' },
+    { group: 'Category', name: 'category:create', description: 'Create new categories' },
+    { group: 'Category', name: 'category:update', description: 'Update categories' },
+    { group: 'Category', name: 'category:view', description: 'View categories' },
+    { group: 'Category', name: 'category:delete', description: 'Delete categories' },
 
     // Payment Method
-    { group: 'payment-method', group: 'payment-method', name: 'payment-method:create', description: 'Create payment methods' },
-    { group: 'payment-method', group: 'payment-method', name: 'payment-method:update', description: 'Update payment methods' },
-    { group: 'payment-method', group: 'payment-method', name: 'payment-method:view', description: 'View payment methods' },
-    { group: 'payment-method', group: 'payment-method', name: 'payment-method:delete', description: 'Delete payment methods' },
+    { group: 'Payment Method', name: 'payment-method:create', description: 'Create payment methods' },
+    { group: 'Payment Method', name: 'payment-method:update', description: 'Update payment methods' },
+    { group: 'Payment Method', name: 'payment-method:view', description: 'View payment methods' },
+    { group: 'Payment Method', name: 'payment-method:delete', description: 'Delete payment methods' },
 
     // Ticket Type
-    { group: 'ticket-type', group: 'ticket-type', name: 'ticket-type:create', description: 'Create ticket types' },
-    { group: 'ticket-type', group: 'ticket-type', name: 'ticket-type:update', description: 'Update ticket types' },
-    { group: 'ticket-type', group: 'ticket-type', name: 'ticket-type:view', description: 'View ticket types' },
-    { group: 'ticket-type', group: 'ticket-type', name: 'ticket-type:delete', description: 'Delete ticket types' },
+    { group: 'Ticket Type', name: 'ticket-type:create', description: 'Create ticket types' },
+    { group: 'Ticket Type', name: 'ticket-type:update', description: 'Update ticket types' },
+    { group: 'Ticket Type', name: 'ticket-type:view', description: 'View ticket types' },
+    { group: 'Ticket Type', name: 'ticket-type:delete', description: 'Delete ticket types' },
 
     // Sale Ticket
-    { group: 'sale-ticket', group: 'sale-ticket', name: 'sale-ticket:view', description: 'View sold tickets' },
+    { group: 'Sale Ticket', name: 'sale-ticket:view', description: 'View sold tickets' },
 
     // Scan Ticket
-    { group: 'scan-ticket', group: 'scan-ticket', name: 'scan-ticket:validate', description: 'Scan and validate tickets' },
+    { group: 'Scan Ticket', name: 'scan-ticket:validate', description: 'Scan and validate tickets' },
 
     // User
-    { group: 'user', group: 'user', name: 'user:create', description: 'Create user accounts' },
-    { group: 'user', group: 'user', name: 'user:update', description: 'Update user accounts' },
-    { group: 'user', group: 'user', name: 'user:view', description: 'View user accounts' },
-    { group: 'user', group: 'user', name: 'user:delete', description: 'Delete user accounts' },
+    { group: 'User', name: 'user:create', description: 'Create user accounts' },
+    { group: 'User', name: 'user:update', description: 'Update user accounts' },
+    { group: 'User', name: 'user:view', description: 'View user accounts' },
+    { group: 'User', name: 'user:delete', description: 'Delete user accounts' },
 
     // Role
-    { group: 'role', group: 'role', name: 'role:create', description: 'Create roles' },
-    { group: 'role', group: 'role', name: 'role:update', description: 'Update roles' },
-    { group: 'role', group: 'role', name: 'role:view', description: 'View roles' },
-    { group: 'role', group: 'role', name: 'role:delete', description: 'Delete roles' },
+    { group: 'Role', name: 'role:create', description: 'Create roles' },
+    { group: 'Role', name: 'role:update', description: 'Update roles' },
+    { group: 'Role', name: 'role:view', description: 'View roles' },
+    { group: 'Role', name: 'role:delete', description: 'Delete roles' },
 
     // Right
-    { group: 'right', group: 'right', name: 'right:view', description: 'View rights and permissions' },
+    { group: 'Right', name: 'right:view', description: 'View rights and permissions' },
 
     // // Permission
-    // { group: 'permission', group: 'permission', name: 'permission:view', description: 'View role permissions' },
-    // { group: 'permission', group: 'permission', name: 'permission:assign', description: 'Assign rights to roles' },
+    // { group: 'Permission', name: 'permission:view', description: 'View role permissions' },
+    // { group: 'Permission', name: 'permission:assign', description: 'Assign rights to roles' },
 
     // Organization
-    { group: 'organization', group: 'organization', name: 'organization:view', description: 'View organizations' },
-    { group: 'organization', group: 'organization', name: 'organization:update', description: 'Update organization status' },
+    { group: 'Organization', name: 'organization:view', description: 'View organizations' },
+    { group: 'Organization', name: 'organization:update', description: 'Update organization status' },
 
     // Request
-    { group: 'request', group: 'request', name: 'request:view', description: 'View organization requests' },
-    { group: 'request', group: 'request', name: 'request:approve', description: 'Approve organization requests' },
-    { group: 'request', group: 'request', name: 'request:reject', description: 'Reject organization requests' },
+    { group: 'Request', name: 'request:view', description: 'View organization requests' },
+    { group: 'Request', name: 'request:approve', description: 'Approve organization requests' },
+    { group: 'Request', name: 'request:reject', description: 'Reject organization requests' },
 ];
 
 
